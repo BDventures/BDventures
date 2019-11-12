eae3cb5cc6d803030643f0648b818aadbe882b47
Daniel Kim <danieljoonkimm@gmail.com>
2019-11-11 18:20:09 -0800

    [edit] making promises in async

--- client/src/components/User/GetUserData.jsx
+++ client/src/components/User/GetUserData.jsx
@@ -6,30 +6,30 @@ const success = (data) => {
   return `${data.name} Kim`
 }
 
-const promise = (data) => {
-  return new Promise ( (resolve, reject) => {
-    let error = `sorry no name is found`
-    if(!data.name) reject(new Error(errorMsg(error)))
+// const promise = (data) => {
+//   return new Promise ( (resolve, reject) => {
+//     let error = `sorry no name is found`
+//     if(!data.name) reject(new Error(errorMsg(error)))
 
-    resolve(success(data))
-  })
-}
+//     resolve(success(data))
+//   })
+// }
 
 //same thing as promise fn? check to see
 
-// const asyncFn = async (data) => {
-//   let error = `sorry no name is found`
-//   if(data) return await errorMsg(error)
+const asyncFn = async (data) => {
+  let error = `sorry no name is found`
+  if(!data) return await errorMsg(error)
 
-//   let awaitedData = await data
-//   return awaitedData
-// }
+  let awaitedData = await success(data)
+  return awaitedData
+}
 
 //if you write async in front of a function, it always returns a promise //wraps non-promises in promises
 //await is the same thing as .then waits until the promise settles and returns its result
 export const GetUserData = async (data) => {
   try {
-    let returnedData = await promise(data)
+    let returnedData = await asyncFn(data)
     console.log(returnedData, 'this is from the try block')
     return returnedData
   }
