9f699ec9a733b021e19836a9dd804d2e27c2d5cb
Daniel Kim <danieljoonkimm@gmail.com>
2019-12-10 17:59:03 -0800

    [edit] fixing hooks

--- client/src/components/WeatherHooks/fetchWeather.jsx
+++ client/src/components/WeatherHooks/fetchWeather.jsx
@@ -11,7 +11,7 @@ export const fetchWeather = (url, loading) => {
       let x = await response.data;
       console.log(x, 'this is x')
       //x.name, x.weather[0].main, x.weather[0].description
-      let weatherTemp = Math.floor((x.main.temp-273.15)*1.8)+32
+      let weatherTemp = await Math.floor((x.main.temp-273.15)*1.8)+32
       console.log(typeof weatherTemp, 'this is weather temp')
       if(x.wind.speed > 3) x.wind.speed = 'Lots of Wind'
       else x.wind.speed = 'Little Bit of Wind'
