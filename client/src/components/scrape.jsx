7064f2d81f0aa282400bec260288e8749acd78f9
Daniel Kim <danieljoonkimm@gmail.com>
2020-02-04 22:31:35 -0800

    [edit] adding tree traversal

--- client/src/components/scrape.jsx
+++ client/src/components/scrape.jsx
@@ -1,58 +1,50 @@
 //tree
 
-class Node {
-  constructor(val) {
-    this.val = val,
-    this.left = null,
-    this.right = null
-  }
-}
+const Node = val => {
+  (this.val = val), (this.left = null), (this.right = null);
+};
 
-class BST {
-  constructor() {
-    this.root = null
-  }
+const BST = () => {
+  this.root = null;
 
-  addNode(val) {
-    let newNode = new Node(val)
+  const addNode = val => {
+    let newNode = new Node(val);
 
-    if(!this.root) this.root = newNode
-
+    if (!root) root = newNode;
     else {
-      this.addNodeToCorrect(this.root, newNode)
+      addNodeToCorrect(this.root, newNode);
     }
-  }
+  };
 
-  addNodeToCorrect(currentNode, newNode) {
-    if(newNode.val < currentNode.val) {
-      if(currentNode.left === null) {
-        currentNode.left = newNode
+  const addNodeToCorrect = (currentNode, newNode) => {
+    if (newNode.val < currentNode.val) {
+      if (currentNode.left === null) {
+        currentNode.left = newNode;
       } else {
-        this.addNodeToCorrect(currentNode.left, newNode)
+        addNodeToCorrect(currentNode.left, newNode);
       }
     } else {
-      if(currentNode.right === null) {
-        currentNode.right = newNode
+      if (currentNode.right === null) {
+        currentNode.right = newNode;
       } else {
-        this.addNodeToCorrect(currentNode.right, newNode)
+        addNodeToCorrect(currentNode.right, newNode);
       }
     }
-  }
+  };
 
-  findTargetNode(currentNode, target) {
-    if(currentNode.target === target) {
-      return true
-    } else {
-      this.findTargetNode(currentNode.left)
-      this.findTargetNode(currentNode.right)
+  const findTargetNode = (currentNode, target) => {
+    if (currentNode.target === target) return true;
+    else {
+      findTargetNode(currentNode.left);
+      findTargetNode(currentNode.right);
     }
-    return false
-  }
-}
+    return false;
+  };
+};
 
-let newBST = new BST()
-newBST.addNode(2)
-newBST.addNode(1)
-newBST.addNode(3)
-newBST.findTargetNode(BST.root, 3)
-console.log(newBST)
+let newBST = new BST();
+newBST.addNode(2);
+newBST.addNode(1);
+newBST.addNode(3);
+newBST.findTargetNode(BST.root, 3);
+console.log(newBST);
