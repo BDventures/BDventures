2eab8a3b91da835ca3637a74a326f9a12dabf851
Daniel Kim <danieljoonkimm@gmail.com>
2020-02-04 22:21:55 -0800

    [edit] fixing tree

--- client/src/components/scrape.jsx
+++ client/src/components/scrape.jsx
@@ -28,8 +28,29 @@ class BST {
       if(currentNode.left === null) {
         currentNode.left = newNode
       } else {
-        
+        this.addNodeToCorrect(currentNode.left, newNode)
+      }
+    } else {
+      if(currentNode.right === null) {
+        currentNode.right = newNode
+      } else {
+        this.addNodeToCorrect(currentNode.right, newNode)
       }
     }
   }
+
+  findTargetNode(currentNode, val) {
+    if(currentNode.val === val) {
+      return true
+    } else {
+      this.findTargetNode(currentNode.left)
+      this.findTargetNode(currentNode.right)
+    }
+  }
 }
+
+let newBST = new BST()
+newBST.addNode(2)
+newBST.addNode(1)
+newBST.addNode(3)
+console.log(newBST)
