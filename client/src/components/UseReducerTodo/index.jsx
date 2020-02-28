7d2cb8db4b1e0fdda03611772a059b6e673b5f55
Daniel Kim <danieljoonkimm@gmail.com>
2020-02-27 22:57:41 -0800

    [edit] fixing

--- client/src/components/UseReducerTodo/index.jsx
+++ client/src/components/UseReducerTodo/index.jsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect, useReducer} from 'react'
 const reducer = (state, action) => {
   switch(action.type) {
     case 'add-todo': return {todos: [...state.todos, {text: action.text}]};
+    
     default: return state
   }
 }
@@ -25,3 +26,61 @@ export const UseReducerTodo = () => {
     </div>
   )
 }
+
+// const messages = (err, success) => {
+//   if(err) console.log(err)
+
+//   console.log(success)
+// }
+
+// const callback = (data, cb) => {
+//   const errorMsg = 'no data to be found'
+//   if(!data) cb(errorMsg)
+
+//   cb(null, messages(data))
+// }
+
+class Node {
+  constructor(value) {
+    this.val = value,
+    this.left = null,
+    this.right = null
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null
+  }
+
+  addNode(val) {
+    let newNode = new Node(val)
+
+    if(!this.root) this.root = newNode
+    
+    else {
+      this.addNodeToCorrect(this.root, newNode)
+    }
+  }
+
+  addNodeToCorrect(node, newNode) {
+    if(newNode.val < node.val) {
+      if(node.left === null) {
+        node.left = newNode
+      } else {
+        this.addNodeToCorrect(node.left, newNode)
+      }
+    } else {
+      if(node.right === null) {
+        node.right = newNode
+      } else {
+        this.addNodeToCorrect(node.right, newNode)
+      }
+    }
+  }
+}
+
+let newBST = new BST()
+newBST.addNode(2)
+newBST.addNode(1)
+newBST.addNode(3)
